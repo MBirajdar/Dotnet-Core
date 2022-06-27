bde57eeaea505be2b91ca55e01376a6336369770
Manoj Birajdar <m.birajdar@zensar.com>
2022-06-27 18:20:59 +0530

    codebaseTest

---
+++ Exam/UniversityForm/UniversityForm/Data/UniversityFormContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UniversityForm.Models;
+
+namespace UniversityForm.Data
+{
+    public class UniversityFormContext : DbContext
+    {
+        public UniversityFormContext (DbContextOptions<UniversityFormContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UniversityForm.Models.Student> Student { get; set; }
+    }
+}
---
+++ Exam/UniversityForm/UniversityForm/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityForm.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string MailId { get; set; }
+
+    }
+}
